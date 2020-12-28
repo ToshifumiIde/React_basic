93abca9a381589073403beda37199dd0e0db94d3
ToshifumiIde <toshifumiide198765@gmail.com>
2020-12-28 09:44:18 +0900

    コンポーネント内でのJS記法、styleの付与

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 
 const App = ()=> {
+
+  const onClickButton = () => {
+    alert();
+  };
+
+  const contentStyle = {
+    color:"blue",
+    fontSize:"24px",
+  };
+  //styleをオブジェクトとして渡すことも可能。
+  //この場合、記法はキャメルケース（cssだとfont-size ）。
+
   return (
     <>
-    <h1>hello world</h1>
-    <p>お元気ですか？</p>
+    <h1 style={{color:"red",}}>hello world</h1>
+    <p  style={contentStyle}>お元気ですか？</p>
+    <button onClick={onClickButton}>
+      ボタン
+    </button>
+    {/* Reactでのイベント設定はキャメルケースで記入 */}
+    {/* この場合、onClick時が関数の実行タイミング()となる */}
+    {/* =の後ろをカーリーブラケット{}で囲うと「中にJSを記載可能」 */}
+    {/* styleの設定も可能。={}はJSの記法を宣言。内部の{}は「オブジェクト」を記載 */}
+    {/* オブジェクトの中に記載する場合、中身は"string"で記載。""で囲わないとエラーが返る */}
     </>//React.Fragment。returnは1つの要素として返却する必要がある。
   )
 };
