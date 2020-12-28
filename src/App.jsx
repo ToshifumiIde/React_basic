3a7600901b488f160d3883a6fec5d8c16c5db708
ToshifumiIde <toshifumiide198765@gmail.com>
2020-12-28 09:21:01 +0900

    初期説明

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-const App = ()=>{
+const App = ()=> {
   return (
+    <>
     <h1>hello world</h1>
+    <p>お元気ですか？</p>
+    </>//React.Fragment。returnは1つの要素として返却する必要がある。
   )
 };
 
 export default App;
+//reactのコンポーネントと明示するため、ファイル名は.jsxで記載することを推奨。
+//命名に関しては「パスカルケース」を推奨(SomeComponent.jsxの様に、単語の頭文字を大文字に設定)
--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />,document.getElementById("root"));
+//reactのレンダリング。
+//第一引数：コンポーネントとしてレンダリングするもの
+//第二引数：どこに反映するか
